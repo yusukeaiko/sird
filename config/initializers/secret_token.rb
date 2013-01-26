23017f8e3d969a0a553ae84fc3cc2c2ca9206e8e
Yusuke AIKO <yusuke.aiko@gmail.com>
2013-01-26 10:53:07 +0900

    initial vertion

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ipl::Application.config.secret_token = '2ac2b3d175955d25f3f859d27a7575e10203cc2e40b4bf060a4087bf72d14a0a3b134d656e93e6dbb9a7ac2ac8dfaba01a5e5dfc8abb88ec8b4b60e602e4d843'
